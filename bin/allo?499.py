9278e757b427a2694a13ce4372e7aec4acd8e332
Florent Vennetier (OpenIO) <florent@fridu.net>
2018-09-18 12:17:05 +0200

    Add allo?499.py
    This script helps reproducing "Connection reset by peer" server errors.

---
+++ bin/allo?499.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+import sys
+import urllib3
+
+
+def read_url(url):
+    http = urllib3.PoolManager()
+    req = http.request('GET', url, preload_content=False)
+    req.read(1024)
+    for kv in sorted(req.info().items()):
+        print("%s: %s" % kv)
+    req.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('usage: %s URL' % sys.argv[0])
+    else:
+        read_url(sys.argv[1])
